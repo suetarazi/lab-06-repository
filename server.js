a8e7027331c93d4ee3989ac842c3f23dfd448480
Anthony Horton <hortonadtwitter@gmail.com>
2020-02-18 13:55:33 -0800

    Half way thru weather data. Parsing darksky.json now.

--- server.js
+++ server.js
@@ -24,14 +24,32 @@ app.get('/location', (request, response) => {
   response.send(dataObj);
 })
 
+app.get('/weather', (request, response) => {
+  let city = request.query.city;
+  let tempData = require('./data/darksky.json');
+  // let dateObj = new Date();
+  // let date = dateObj.toDateString();
+  let tempObj = tempData.daily.data[0];
+  // let dataObj = [];
+  for (let i = 0; i < tempObj.length; i++) {
+    // dataObj.push(new Sky(tempData));
+  }
+  response.send(dataObj);
+})
 
-function City (city, obj){
+function City(city, obj) {
   this.search_query = city;
   this.formatted_query = obj.display_name;
   this.latitude = obj.lat;
   this.longitude = obj.lon;
 }
 
-app.listen(PORT, () => { 
+function Sky(obj) {
+  this.forecast = 
+  this.time =
+}
+
+
+app.listen(PORT, () => {
   console.log(`${PORT}`);
 })
