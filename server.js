e22fe28ac7c1348f6ea1559cc136cf2b1dfeddc7
suetarazi <suemachtley@gmail.com>
2020-02-18 13:51:50 -0800

    attempting to create weather app

--- server.js
+++ server.js
@@ -9,17 +9,15 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-    console.log('listening to ${PORT}');
-})
 
-app.get('/location', (require, Response)=>{
+app.get('/location', (request, response)=>{
 try{
     let city = request.query.city;
+    console.log(city);
     let geoData = require('./data/geo.json');
-
     let location = new City(city, geoData[0]);
-    Response.send(location);
+    console.log(location);
+    response.send(location);
 }
 catch(err){
     console.log(err);
@@ -32,3 +30,28 @@ function City (city, obj){
     this.latitude = obj.lat;
     this.longitude = obj.lon;
 }
+
+app.get('/weather', request, response) => {
+    let weather = request.query.city;
+    console.log(weather);
+    let skyData = require ('./data/darksky.json');
+    let weatherObj = weather.daily[1];
+    
+    // for (const i=0; i<obj.daily.data.length; i<8){
+    // weatherObj.push(new Weather (darksky[i]));
+    }
+    console.log(forecast, time);
+    response.send(forecast, time);
+}
+
+function Weather(city, obj){
+    this.search_query = city;
+    this.forecast = obj.daily.data[i].summary; 
+    let date = new Date (obj.daily.data[i].time);
+    this.time = date.toDateString(); 
+}
+
+
+app.listen(PORT, () => {
+    console.log(`listening to ${PORT}`);
+})
