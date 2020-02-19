8972ac63db294b570797e7661ba814781c54f3cf
Anthony Horton <hortonadtwitter@gmail.com>
2020-02-19 13:33:37 -0800

    Added API call for eventful API to include JSON stringify. Need to rework object parsing

--- server.js
+++ server.js
@@ -18,17 +18,19 @@ const THEMOVIEDB_API_KEY = process.env.THEMOVIEDB;
 const EVENTFUL_API_KEY = process.env.EVENTFUL;
 
 app.get('/location', locationHandler);
+app.get('/weather', weatherHandler);
+app.get('/events', partyHandler);
 // app.get('/location', locationHandler);
 // app.get('/location', locationHandler);
-// app.get('/location', locationHandler);
-// app.get('/location', locationHandler);
+
+let mapCoordinates = "";
 
 function locationHandler(request, response) {
   // console.log(request.query);
   let city = request.query.city;
   // let geoData = require('./data/geo.json');
   const url = `https://us1.locationiq.com/v1/search.php?key=${GEOCODE_API_KEY}&q=${city}&format=json`;
-  console.log(url);
+  // console.log(url);
 
   superagent.get(url)
     .then(data => {
@@ -37,14 +39,45 @@ function locationHandler(request, response) {
       response.send(dataObj);
     })
     .catch(() => {
-      errorHandler('There was an error', request, response);
+      console.error('There was an error', request, response);
     });
-  //   let dataObj = new City(city, geoData[0]);
-  //   console.log(dataObj);
-  //   response.send(dataObj);
-  // } catch (err) {
-  //   console.log(err);
-  // }
+}
+
+function weatherHandler(request, response) {
+  let reqData = request.query;
+  // let geoData = require('./data/geo.json');
+  const url = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${reqData.latitude},${reqData.longitude}`;
+  // const url = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${mapCoordinates}`;
+  // console.log(url);
+
+  superagent.get(url)
+    .then(tempData => {
+      const weatherData = tempData.body.daily.data;
+      const dataObj = weatherData.map(day => new Sky(day));
+      response.send(dataObj);
+    })
+    .catch(() => {
+      console.error('There was an error', request, response);
+    });
+}
+
+function partyHandler(request, response) {
+  console.log(request.query);
+  let reqData = request.query;
+  // // let geoData = require('./data/geo.json');
+  const url = `http://api.eventful.com/rest/events/search?app_key=${EVENTFUL_API_KEY}&where=${reqData.latitude},${reqData.longitude}&within=25`;
+  console.log(url);
+
+  superagent.get(url)
+    .then(eventful => {
+      const eventData = JSON.stringify(eventful.body.events);
+      console.log(eventData);
+      const dataObj = eventData.map(day => new Party(day));
+      response.send(dataObj);
+    })
+    .catch(() => {
+      console.error('There was an error', request, response);
+    });
 }
 
 // app.get('/weather', (request, response) => {
@@ -74,10 +107,17 @@ function City(city, obj) {
 }
 
 function Sky(obj) {
+  // console.log("/./././." + obj.summary);
   this.forecast = obj.summary;
-  this.time = new Date(obj.time).toDateString();
+  this.time = new Date(obj.time*1000).toDateString();
 }
 
+function Party(obj){
+  this.link = obj.venue_url;
+  this.name = obj.title;
+  this.event_date = obj.start_time;
+  this.summary = obj.description;
+}
 
 app.listen(PORT, () => {
   console.log(`${PORT}`);
